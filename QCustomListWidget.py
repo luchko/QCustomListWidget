9c1cb2ff0e6414f35182415a247c36ebb2e146ce
Luchko <luchko.ivan@gmail.com>
2017-05-07 02:33:13 +0300

    ref to github added

--- QCustomListWidget.py
+++ QCustomListWidget.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb 25 17:07:36 2017
-
+Licensed under the terms of the MIT License 
+https://github.com/luchko/QCustomListWidget
 @author: Ivan Luchko (luchko.ivan@gmail.com)
 
 This module contains the definition of the QCustomListWidget with add/remove buttons.
